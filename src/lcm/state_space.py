c9bef03fb5bac9caa1eb383d536ec32f5c37d6c0
Janos Gabler <janos.gabler@gmail.com>
2022-11-18 18:40:41 +0100

    More documentation.

--- src/lcm/state_space.py
+++ src/lcm/state_space.py
@@ -17,7 +17,20 @@ from lcm.dispatchers import spacemap
 
 
 class IndexerInfo(NamedTuple):
-    """Information needed to work with an indexer array."""
+    """Information needed to work with an indexer array.
+
+    In particular, this contains enough information to wrap an indexer array into a
+    function that can be understood by dags.
+
+    Attributes:
+        axis_names (list): List of strings containing the names of the axes of the
+            indexer array.
+        name (str): The name of the indexer array. This will become an argument name
+            of the function we need for dags.
+        out_name (str): The name of the result of indexing into the indexer. This will
+            become the name of the function we need for dags.
+
+    """
 
     axis_names: List[str]
     name: str
@@ -25,7 +38,16 @@ class IndexerInfo(NamedTuple):
 
 
 class Grid(NamedTuple):
-    """Information needed to define or interpret a grid."""
+    """Information needed to define or interpret a grid.
+
+    Attributes:
+        kind (str): Name of a grid type implemented in lcm.grids.
+        specs (dict, np.ndarray): Specification of the grid. E.g. {"start": float,
+            "stop": float, "n_points": int} for a linspace.
+        name (str): Name of the grid. This will be axis names in an array that
+            represents a cartesian product of one dimensional grids.
+
+    """
 
     kind: str
     specs: Union[dict, np.ndarray]
@@ -33,14 +55,34 @@ class Grid(NamedTuple):
 
 
 class Space(NamedTuple):
-    """Everything needed to evaluate a function on a space (e.g. state space)."""
+    """Everything needed to evaluate a function on a space (e.g. state space).
+
+    Attributes:
+        sparse_vars (dict): Dictionary containing the names of sparse variables as keys
+            and arrays with values of those variables as values. Together, the arrays
+            define all feasible combinations of sparse variables.
+        dense_vars (dict): Dictionary containing one dimensional grids of
+            dense variables.
+
+    """
 
     sparse_vars: Dict
     dense_vars: Dict
 
 
 class SpaceInfo(NamedTuple):
-    """Everything needed to work with the output of a function evaluated on a space."""
+    """Everything needed to work with the output of a function evaluated on a space.
+
+    Attributes:
+        axis_names (list): List with axis names of an array that contains function
+            values for all elements in a space.
+        lookup_info (dict): Dict that defines the possible labels of all discrete
+            variables and their order.
+        interpolation_info (dict): Dict that defines information on the grids of all
+            continuous variables.
+        indexer_infos (list): List of IndexerInfo objects.
+
+    """
 
     axis_names: List[str]
     lookup_info: Dict[str, List[str]]
