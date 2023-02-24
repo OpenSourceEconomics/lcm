268a692041bcfefaab948390d62364151af745c4
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2023-02-24 18:01:22 +0100

    Don't let test hurt the eyes.

--- tests/test_create_params.py
+++ tests/test_create_params.py
@@ -6,14 +6,14 @@ from pybaum import leaf_names
 def test_create_params_phelps_deaton_with_shocks():
     params = create_params(PHELPS_DEATON_WITH_SHOCKS)
 
-    names = leaf_names(params, separator="$")
+    names = leaf_names(params, separator="__")
     expected_names = [
         "beta",
-        "utility$delta",
-        "next_wealth$interest_rate",
-        "next_wealth$wage",
-        "wage_shock$sd",
-        "additive_utility_shock$scale",
+        "utility__delta",
+        "next_wealth__interest_rate",
+        "next_wealth__wage",
+        "wage_shock__sd",
+        "additive_utility_shock__scale",
     ]
 
     assert sorted(names) == sorted(expected_names)
