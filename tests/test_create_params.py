756357ca014277ca44681c317619dbb982a1bb9c
timmens <mensingertim@gmail.com>
2023-05-09 14:13:23 +0200

    Add testing for create_params.py

--- tests/test_create_params.py
+++ tests/test_create_params.py
@@ -1,4 +1,9 @@
-from lcm.create_params import create_params
+import numpy as np
+from lcm.create_params import (
+    _create_function_params,
+    _create_shock_params,
+    create_params,
+)
 from lcm.example_models import PHELPS_DEATON_WITH_SHOCKS
 from pybaum import leaf_names
 
@@ -17,3 +22,47 @@ def test_create_params_phelps_deaton_with_shocks():
     ]
 
     assert sorted(names) == sorted(expected_names)
+
+
+def test_create_params_without_shocks():
+    model = {
+        "functions": {
+            "f": lambda a, b, c: None,  # noqa: ARG005
+        },
+        "choices": {
+            "a": None,
+        },
+        "states": {
+            "b": None,
+        },
+    }
+    got = create_params(model)
+    assert got == {"beta": np.nan, "f": {"c": np.nan}}
+
+
+def test_create_function_params():
+    model = {
+        "functions": {
+            "f": lambda a, b, c: None,  # noqa: ARG005
+        },
+        "choices": {
+            "a": None,
+        },
+        "states": {
+            "b": None,
+        },
+    }
+    got = _create_function_params(model)
+    assert got == {"f": {"c": np.nan}}
+
+
+def test_create_shock_params():
+    shocks = {
+        "a": "lognormal",
+        "b": "extreme_value",
+    }
+    got = _create_shock_params(shocks)
+
+    assert {"a", "b"} == set(got.keys())
+    assert got["a"] == {"sd": np.nan}
+    assert got["b"] == {"scale": np.nan}
