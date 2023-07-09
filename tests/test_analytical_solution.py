e7edc29ecd26a06ef7beab8d31ad8a7dc3be154e
Marvin Immesberger <immesberger@posteo.de>
2023-07-09 22:54:59 +0200

    Add new tests data and adjust test to central model definition via get_model.

--- tests/test_analytical_solution.py
+++ tests/test_analytical_solution.py
@@ -5,71 +5,20 @@ from pathlib import Path
 import numpy as np
 import pytest
 from lcm.entry_point import get_lcm_function
-from lcm.example_models import PHELPS_DEATON_NO_BORROWING
+from lcm.get_model import get_model
 from numpy.testing import assert_array_almost_equal as aaae
-from pybaum import tree_update
 
 DATA = Path(__file__).parent.resolve().joinpath("analytical_solution")
 
 
-@pytest.fixture()
-def test_model():
-    config_update = {
-        "n_periods": 5,
-        "choices": {
-            "consumption": {
-                "n_points": 10_000,
-            },
-        },
-        "states": {
-            "wealth": {
-                "n_points": 10_000,
-            },
-        },
-    }
-    return tree_update(PHELPS_DEATON_NO_BORROWING, config_update)
-
-
 TEST_CASES = {
-    "iskhakov_2017": {
-        # Tests against the analytical solution by Iskhakov et al (2017).
-        "beta": 0.98,
-        "next_wealth": {
-            "wage": 20.0,
-            "interest_rate": 0.0,
-        },
-        "utility": {
-            "delta": 1.0,
-        },
-    },
-    "low_delta": {
-        # For very low values of delta we expect that most individuals work their entire
-        # life.
-        "beta": 0.98,
-        "next_wealth": {
-            "wage": 20.0,
-            "interest_rate": 0.0,
-        },
-        "utility": {
-            "delta": 0.1,
-        },
-    },
-    "high_wage": {
-        # For high wage we ...
-        "beta": 0.98,
-        "next_wealth": {
-            "wage": 100.0,
-            "interest_rate": 0.0,
-        },
-        "utility": {
-            "delta": 0.1,
-        },
-    },
+    "iskhakov_2017_five_periods": get_model("iskhakov_2017_five_periods"),
+    "iskhakov_2017_low_delta": get_model("iskhakov_2017_low_delta"),
 }
 
 
-@pytest.mark.parametrize(("test_id", "params"), TEST_CASES.items())
-def test_analytical_solution(test_id, params, test_model):
+@pytest.mark.parametrize(("test_id", "model"), TEST_CASES.items())
+def test_analytical_solution_values(test_id, model):
     """Test that the numerical solution matches the analytical solution.
 
     The analytical solution is from Iskhakov et al (2017) and is generated
@@ -79,18 +28,11 @@ def test_analytical_solution(test_id, params, test_model):
     with DATA.joinpath(f"{test_id}_v.pkl").open("rb") as file:
         analytical = pickle.load(file)  # noqa: S301
 
-    if test_id == "low_delta":
-        # nothing intersting happens in periods 4 and 5, so we skip them
-        # to save runtime
-        test_model["n_periods"] = 3
-
     # Prepare config parameters
-    solve_model, params_template = get_lcm_function(model=test_model)
-
-    params = tree_update(params_template, params)
+    solve_model, _ = get_lcm_function(model=model.model)
 
     # Solve model using LCM
-    vf_arr_list = solve_model(params=params)
+    vf_arr_list = solve_model(params=model.params)
     numerical_solution = np.stack(vf_arr_list)
 
     numerical = {
