45e76f6764e32a042e9c8ca0110c9c348f78e5b3
timmens <mensingertim@gmail.com>
2023-08-21 16:04:41 +0200

    Add test that tries to solve the parttime human capital model

---
+++ tests/test_model_parttime_human_capital.py
@@ -0,0 +1,12 @@
+from lcm.model_parttime_human_capital import PARTTIME_HUMAN_CAPITAL, PARTTIME_HUMAN_CAPITAL_PARAMS
+from lcm.entry_point import get_lcm_function
+
+
+def test_model_parttime_human_capital_solve():
+
+    solve, params = get_lcm_function(
+        model=PARTTIME_HUMAN_CAPITAL,
+        targets="solve"
+    )
+
+    solve(params=PARTTIME_HUMAN_CAPITAL_PARAMS)
