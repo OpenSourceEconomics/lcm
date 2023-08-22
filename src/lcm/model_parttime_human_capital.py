Fix bug in choice_axes calculation

src/lcm/model_parttime_human_capital.py
@@ -28,6 +28,7 @@ WORKING_HOURS_CATS = [0, PART_TIME_WORKING_HOURS, FULL_TIME_WORKING_HOURS]
 def utility(
     crra_value_of_consumption,
     disutility_of_working,
+    human_capital,  # noqa: ARG001
 ):
     """Utility.
 
@@ -113,10 +114,6 @@ def wage(
     return jnp.exp(log_wage)
 
 
-def wage_constraint(wage, human_capital):
-    return jnp.logical_and(wage > 0, human_capital > 0)
-
-
 # Human capital state transition
 # ======================================================================================
 
@@ -193,7 +190,6 @@ PARTTIME_HUMAN_CAPITAL = {
         "next_wealth": next_wealth,
         "consumption_constraint": consumption_constraint,
         "age": age,
-        "wage_constraint": wage_constraint,
     },
     "choices": {
         "working_hours": {"options": WORKING_HOURS_CATS},
@@ -246,5 +242,4 @@ PARTTIME_HUMAN_CAPITAL_PARAMS = {
         "depreciation": 0.9,
         "experience_factor_part_time": 0.5,
     },
-    "wage_constraint": {},
 }

tests/test_model_parttime_human_capital.py
@@ -1,3 +1,4 @@
+import jax.numpy as jnp
 from lcm.entry_point import get_lcm_function
 from lcm.model_parttime_human_capital import (
     PARTTIME_HUMAN_CAPITAL,
@@ -7,5 +8,21 @@ from lcm.model_parttime_human_capital import (
 
 def test_model_parttime_human_capital_solve():
     solve, params = get_lcm_function(model=PARTTIME_HUMAN_CAPITAL, targets="solve")
-
     solve(params=PARTTIME_HUMAN_CAPITAL_PARAMS)
+
+
+def test_model_parttime_human_capital_solve_and_simulate():
+    initial_states = {
+        "human_capital": jnp.array([0.5]),
+        "wealth": jnp.array([500.0]),
+    }
+
+    solve_and_simulate, params = get_lcm_function(
+        model=PARTTIME_HUMAN_CAPITAL,
+        targets="solve_and_simulate",
+    )
+
+    solve_and_simulate(
+        params=PARTTIME_HUMAN_CAPITAL_PARAMS,
+        initial_states=initial_states,
+    )