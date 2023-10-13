5c34cecc4a8d58cfb2083999455e57940dcda53b
Tim <tim@mensinger96.com>
2023-10-13 16:42:56 +0200

    Jit compute_ccv

--- src/lcm/entry_point.py
+++ src/lcm/entry_point.py
@@ -128,7 +128,7 @@ def get_lcm_function(model, targets="solve", interpolation_options=None):
             utility_and_feasibility=u_and_f,
             continuous_choice_variables=list(_choice_grids),
         )
-        compute_ccv_functions.append(compute_ccv)
+        compute_ccv_functions.append(jax.jit(compute_ccv))
 
         compute_ccv_argmax = create_compute_conditional_continuation_policy(
             utility_and_feasibility=u_and_f,
@@ -160,7 +160,7 @@ def get_lcm_function(model, targets="solve", interpolation_options=None):
         compute_ccv_functions=compute_ccv_functions,
         emax_calculators=emax_calculators,
     )
-    solve_model = jax.jit(_solve_model)
+    solve_model = _solve_model
 
     _next_state_simulate = get_next_state_function(model=_mod, target="simulate")
     simulate_model = partial(
