68c20dd3ce33ae80adfe81b4924e44df64444aaa
Tim Mensinger <mensingertim@gmail.com>
2024-04-11 14:59:55 +0200

    Update create_params.py (#66)

--- src/lcm/create_params.py
+++ src/lcm/create_params.py
@@ -1,47 +1,66 @@
 """Create a parameters for a model specification."""
 
 import inspect
+from typing import Any
 
-import numpy as np
+import jax.numpy as jnp
+import pandas as pd
+from jax import Array
 
 
-def create_params(user_model, variable_info, grids):
-    """Get parameters from a model specification.
+def create_params_template(
+    model_spec: dict[str, Any],
+    variable_info: pd.DataFrame,
+    grids: dict[str, Array],
+    default_params: dict[str, float] | None = None,
+) -> dict[str, dict[str, Any] | float]:
+    """Create parameter template from a model specification.
 
     Args:
-        user_model (dict): A model specification. Has keys
+        model_spec (dict): A model specification provided by the user. Has keys
             - "functions": A dictionary of functions used in the model.
             - "choices": A dictionary of choice variables.
             - "states": A dictionary of state variables.
             - "n_periods": Number of periods in the model (int).
             - "shocks": A dictionary of shock variables (optional).
         variable_info (pd.DataFrame): A dataframe with information about the variables.
-        grids (dict): A dictionary of grids.
+        grids (dict): A dictionary of grids consistent with model_spec..
+        default_params (dict): A dictionary of default parameters. Default is None. If
+            None, the default {"beta": np.nan} is used. For other lifetime reward
+            objectives, additional parameters may be required, for example {"beta":
+            np.nan, "delta": np.nan} for beta-delta discounting.
 
     Returns:
-        dict: A dictionary of model parameters.
+        dict: A nested dictionary of model parameters.
 
     """
-    params = _create_standard_params() | _create_function_params(user_model)
+    if default_params is None:
+        # The default lifetime reward objective in LCM is expected discounted utility.
+        # For this objective the only additional parameter is the discounting rate beta.
+        default_params = {"beta": jnp.nan}
+
+    params_template = default_params | _create_function_params(model_spec)
 
     if variable_info["is_stochastic"].any():
-        params["shocks"] = _create_shock_params(
-            user_model,
+        params_template["shocks"] = _create_stochastic_transition_params(
+            model_spec=model_spec,
             variable_info=variable_info,
             grids=grids,
         )
 
-    return params
+    return params_template
 
 
-def _create_function_params(model):
+def _create_function_params(model_spec: dict[str, Any]) -> dict[str, dict[str, Any]]:
     """Get function parameters from a model specification.
 
-    The argument '_period' is handled separately. It is not treated as a parameter, but
-    as the period of the model. Functions like 'age' can depend on it.
+    Explanation: We consider the arguments of all model functions, from which we exclude
+    all variables that are states, choices or the period argument. Everything else is
+    considered a parameter of the respective model function that is provided by the
+    user.
 
     Args:
-        model (dict): A model specification. Has keys
+        model_spec (dict): A model specification provided by the user. Has keys
             - "functions": A dictionary of functions used in the model.
             - "choices": A dictionary of choice variables.
             - "states": A dictionary of state variables.
@@ -49,90 +68,107 @@ def _create_function_params(model):
             - "shocks": A dictionary of shock variables (optional).
 
     Returns:
-        dict: A dictionary of function parameters.
+        dict: A dictionary for each model function, containing a parameters required in
+            the model functions, initialized with jnp.nan.
 
     """
+    # Collect all model variables, that includes choices, states, the period, and
+    # auxiliary variables (model function names).
     variables = {
-        *model["functions"],
-        *model["choices"],
-        *model["states"],
+        *model_spec["functions"],
+        *model_spec["choices"],
+        *model_spec["states"],
+        "_period",
     }
 
-    if "shocks" in model:
-        variables = variables | set(model["shocks"])
-    out = {}
-    for name, func in model["functions"].items():
+    if "shocks" in model_spec:
+        variables = variables | set(model_spec["shocks"])
+
+    function_params = {}
+    # For each model function, capture the arguments of the function that are not in the
+    # set of model variables, and initialize them.
+    for name, func in model_spec["functions"].items():
         arguments = set(inspect.signature(func).parameters)
         params = sorted(arguments.difference(variables))
-        out[name] = {p: np.nan for p in params if p != "_period"}
-    return out
+        function_params[name] = {p: jnp.nan for p in params}
+
+    return function_params
 
 
-def _create_shock_params(model, variable_info, grids):
-    """Infer parameters from shocks.
+def _create_stochastic_transition_params(
+    model_spec: dict[str, Any],
+    variable_info: pd.DataFrame,
+    grids: dict[str, Array],
+) -> dict[str, Array]:
+    """Create parameters for stochastic transitions.
 
     Args:
-        model (dict): A model specification. Has keys
+        model_spec (dict): A model specification provided by the user. Has keys
             - "functions": A dictionary of functions used in the model.
             - "choices": A dictionary of choice variables.
             - "states": A dictionary of state variables.
             - "n_periods": Number of periods in the model (int).
             - "shocks": A dictionary of shock variables (optional).
         variable_info (pd.DataFrame): A dataframe with information about the variables.
-        grids (dict): A dictionary of grids.
+        grids (dict): A dictionary of grids consistent with model_spec.
 
     Returns:
-        dict: A dictionary of parameters.
+        dict: A dictionary of parameters required for stochastic transitions,
+            initialized with jnp.nan matrices of the correct dimensions.
 
     """
     stochastic_variables = variable_info.query("is_stochastic").index.tolist()
 
-    for var in stochastic_variables:
-        if (
-            not variable_info.loc[var, "is_state"]
-            or not variable_info.loc[var, "is_discrete"]
-        ):
-            raise ValueError(
-                f"Shock {var} is stochastic but not a discrete state variable.",
-            )
+    # ----------------------------------------------------------------------------------
+    # Assert that all stochastic variables are discrete state variables
+    # ----------------------------------------------------------------------------------
+    discrete_state_vars = set(variable_info.query("is_state & is_discrete").index)
 
-    params = {}
-    for var in stochastic_variables:
-        # read signature of next function corresponding to stochastic variable
-        dependencies = list(
-            inspect.signature(model["functions"][f"next_{var}"]).parameters,
+    if invalid := set(stochastic_variables) - discrete_state_vars:
+        raise ValueError(
+            f"The following variables are stochastic, but are not discrete state "
+            f"variables: {invalid}. This is currently not supported.",
         )
 
-        _check_variables_are_all_discrete_or_period(
-            variables=dependencies,
-            variable_info=variable_info,
-            msg_suffix=(
-                f"The function next_{var} can only depend on discrete variables or"
-                f"'_period'."
-            ),
+    # ----------------------------------------------------------------------------------
+    # Create template matrices for stochastic transitions
+    # ----------------------------------------------------------------------------------
+
+    # Stochastic transition functions can only depend on discrete vars or '_period'.
+    valid_vars = set(variable_info.query("is_discrete").index) | {"_period"}
+
+    stochastic_transition_params = {}
+    invalid_dependencies = {}
+
+    for var in stochastic_variables:
+
+        # Retrieve corresponding next function and its arguments
+        next_var = model_spec["functions"][f"next_{var}"]
+        dependencies = list(inspect.signature(next_var).parameters)
+
+        # If there are invalid dependencies, store them in a dictionary and continue
+        # with the next variable to collect as many invalid arguments as possible.
+        if invalid := set(dependencies) - valid_vars:
+            invalid_dependencies[var] = invalid
+        else:
+            # Get the dimensions of variables that influence the stochastic variable
+            dimensions_of_deps = [
+                len(grids[arg]) if arg != "_period" else model_spec["n_periods"]
+                for arg in dependencies
+            ]
+            # Add the dimension of the stochastic variable itself at the end
+            dimensions = (*dimensions_of_deps, len(grids[var]))
+
+            stochastic_transition_params[var] = jnp.full(dimensions, jnp.nan)
+
+    # ----------------------------------------------------------------------------------
+    # Raise an error if there are invalid arguments
+    # ----------------------------------------------------------------------------------
+    if invalid_dependencies:
+        raise ValueError(
+            f"Stochastic transition functions can only depend on discrete variables or "
+            "'_period'. The following variables have invalid arguments: "
+            f"{invalid_dependencies}.",
         )
 
-        # get dimensions of variables that influence the stochastic variable
-        dimensions = [
-            len(grids[dep]) if dep != "_period" else model["n_periods"]
-            for dep in dependencies
-        ]
-        # add dimension of stochastic variable to last axis
-        dimensions = (*dimensions, len(grids[var]))
-
-        params[var] = np.full(dimensions, np.nan)
-
-    return params
-
-
-def _create_standard_params():
-    return {"beta": np.nan}
-
-
-def _check_variables_are_all_discrete_or_period(variables, variable_info, msg_suffix):
-    discrete_vars = variable_info.query("is_discrete").index.tolist()
-    for var in variables:
-        if var not in discrete_vars and var != "_period":
-            raise ValueError(
-                f"Variable {var} is not a discrete variable. {msg_suffix}",
-            )
+    return stochastic_transition_params
--- tests/test_create_params.py
+++ tests/test_create_params.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+import pytest
 from lcm.create_params import (
     _create_function_params,
-    _create_shock_params,
-    create_params,
+    _create_stochastic_transition_params,
+    create_params_template,
 )
 from numpy.testing import assert_equal
 
@@ -20,7 +21,7 @@ def test_create_params_without_shocks():
             "b": None,
         },
     }
-    got = create_params(
+    got = create_params_template(
         model,
         variable_info=pd.DataFrame({"is_stochastic": [False]}),
         grids=None,
@@ -45,7 +46,7 @@ def test_create_function_params():
 
 
 def test_create_shock_params():
-    def next_a(a):  # noqa: ARG001
+    def next_a(a, _period):  # noqa: ARG001
         pass
 
     variable_info = pd.DataFrame(
@@ -53,9 +54,47 @@ def test_create_shock_params():
         index=["a"],
     )
 
-    got = _create_shock_params(
-        model={"functions": {"next_a": next_a}},
+    got = _create_stochastic_transition_params(
+        model_spec={"functions": {"next_a": next_a}, "n_periods": 3},
         variable_info=variable_info,
         grids={"a": np.array([1, 2])},
     )
-    assert_equal(got["a"], np.full((2, 2), np.nan))
+    assert_equal(got["a"], np.full((2, 3, 2), np.nan))
+
+
+def test_create_shock_params_invalid_variable():
+    def next_a(a):  # noqa: ARG001
+        pass
+
+    variable_info = pd.DataFrame(
+        {"is_stochastic": True, "is_state": True, "is_discrete": False},
+        index=["a"],
+    )
+
+    with pytest.raises(ValueError, match="The following variables are stochastic, but"):
+        _create_stochastic_transition_params(
+            model_spec={"functions": {"next_a": next_a}},
+            variable_info=variable_info,
+            grids={"a": np.array([1, 2])},
+        )
+
+
+def test_create_shock_params_invalid_dependency():
+    def next_a(a, b, _period):  # noqa: ARG001
+        pass
+
+    variable_info = pd.DataFrame(
+        {
+            "is_stochastic": [True, False],
+            "is_state": [True, False],
+            "is_discrete": [True, False],
+        },
+        index=["a", "b"],
+    )
+
+    with pytest.raises(ValueError, match="Stochastic transition functions can only"):
+        _create_stochastic_transition_params(
+            model_spec={"functions": {"next_a": next_a}},
+            variable_info=variable_info,
+            grids={"a": np.array([1, 2])},
+        )
