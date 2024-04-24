79e657fb3f05ab190311cd026a2d4f0b65e18520
mj023 <max.jahn45@gmail.com>
2024-04-24 16:46:07 +0200

    Add long test

---
+++ tests/test_long.py
@@ -0,0 +1,22 @@
+import pytest
+import timeit
+import jax
+import nvtx
+from lcm.entry_point import get_lcm_function
+from lcm.example_models.example_models_long import PARAMS, PHELPS_DEATON
+
+SKIP_REASON = """The test is designed to run approximately 1 minute on a standard
+laptop, such that we can differentiate the performance of running LCM on a GPU versus
+on the CPU."""
+
+
+def test_long():
+    
+    solve_model, template = get_lcm_function(PHELPS_DEATON, targets="solve")
+    with nvtx.annotate("solve_model", color="blue"):
+        solve_model(PARAMS)
+
+#jax.profiler.start_trace("/tmp/tensorboard")
+test_long()
+#jax.profiler.stop_trace()
+
