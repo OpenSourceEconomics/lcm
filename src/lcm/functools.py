14bd13995633c2a18ad2acc7e774da45bc2dcb53
Tim Mensinger <mensingertim@gmail.com>
2024-06-12 17:26:40 +0200

    Update functools.py

--- src/lcm/functools.py
+++ src/lcm/functools.py
@@ -1,7 +1,7 @@
 import functools
 import inspect
 from collections.abc import Callable
-from typing import Any, TypeVar
+from typing import Any, TypeVar, cast
 
 F = TypeVar("F", bound=Callable[..., Any])
 
@@ -31,12 +31,7 @@ def allow_only_kwargs(func: F) -> F:
 
     # Create new signature without positional-only arguments
     new_parameters = [
-        (
-            p.replace(kind=inspect.Parameter.POSITIONAL_OR_KEYWORD)
-            if p.kind == inspect.Parameter.POSITIONAL_ONLY
-            else p
-        )
-        for p in parameters.values()
+        p.replace(kind=inspect.Parameter.KEYWORD_ONLY) for p in parameters.values()
     ]
     new_signature = signature.replace(parameters=new_parameters)
 
@@ -51,10 +46,14 @@ def allow_only_kwargs(func: F) -> F:
             )
 
         if extra := set(kwargs).difference(parameters):
-            raise ValueError(f"Expected arguments: {parameters}, got extra: {extra}")
+            raise ValueError(
+                f"Expected arguments: {list(parameters)}, got extra: {extra}",
+            )
 
         if missing := set(parameters).difference(kwargs):
-            raise ValueError(f"Expected arguments: {parameters}, missing: {missing}")
+            raise ValueError(
+                f"Expected arguments: {list(parameters)}, missing: {missing}",
+            )
 
         # Retrieve keyword-only arguments
         kw_only_kwargs = {k: kwargs[k] for k in kw_only_parameters}
@@ -70,62 +69,21 @@ def allow_only_kwargs(func: F) -> F:
     # This raises a mypy error but is perfectly fine to do. See
     # https://github.com/python/mypy/issues/12472
     func_with_only_kwargs.__signature__ = new_signature  # type: ignore[attr-defined]
-    return func_with_only_kwargs
+
+    # We cast to F here to signal mypy that the return type is the same as the input
+    # type. This ignores the change of parameters from positional to keyword-only
+    # arguments.
+    # TODO(@timmens): Remove this cast once we find an explicit way to specify the
+    # change from positional to keyword-only parameter in the function signature
+    # https://github.com/OpenSourceEconomics/lcm/issues/80.
+    return cast(F, func_with_only_kwargs)
 
 
-def allow_kwargs(func):
-    """Allow a function to be called with keyword arguments.
-
-    Args:
-        func (Callable): The function to be wrapped.
-
-    Returns:
-        Callable: A Callable with the same arguments as func (but with the additional
-            possibility to call it with keyword arguments).
-
-    """
-    signature = inspect.signature(func)
-    parameters = signature.parameters
-
-    # Get names of keyword-only arguments
-    kw_only_parameters = [
-        p.name for p in parameters.values() if p.kind == inspect.Parameter.KEYWORD_ONLY
-    ]
-
-    # Create new signature without positional-only arguments
-    new_parameters = [
-        (
-            p.replace(kind=inspect.Parameter.POSITIONAL_OR_KEYWORD)
-            if p.kind == inspect.Parameter.POSITIONAL_ONLY
-            else p
-        )
-        for p in parameters.values()
-    ]
-    new_signature = signature.replace(parameters=new_parameters)
-
-    @functools.wraps(func)
-    def allow_kwargs_wrapper(*args, **kwargs):
-        # Check if the total number of arguments matches the function signature
-        if len(args) + len(kwargs) != len(parameters):
-            raise ValueError(_error_message(args, kwargs, parameters))
-
-        # Retrieve keyword-only arguments
-        kw_only_kwargs = {k: kwargs[k] for k in kw_only_parameters}
-
-        # Get kwargs that must be converted to positional arguments
-        pos_kwargs = {k: v for k, v in kwargs.items() if k not in kw_only_parameters}
-
-        # Collect all positional arguments in correct order
-        positional = list(args) + convert_kwargs_to_args(pos_kwargs, list(parameters))
-
-        return func(*positional, **kw_only_kwargs)
-
-    allow_kwargs_wrapper.__signature__ = new_signature
-    return allow_kwargs_wrapper
-
-
-def allow_args(func):
+def allow_args(func: F) -> F:
     """Allow a function to be called with positional arguments.
+
+    In comparison to `allow_only_kwargs`, the `allow_args` decorator does not enforce
+    that the function is called only with positional arguments.
 
     Args:
         func (Callable): The function to be wrapped.
@@ -158,7 +116,13 @@ def allow_args(func):
     def allow_args_wrapper(*args, **kwargs):
         # Check if the total number of arguments matches the function signature
         if len(args) + len(kwargs) != len(parameters):
-            raise ValueError(_error_message(args, kwargs, parameters))
+            too_many = len(args) + len(kwargs) > len(parameters)
+            msg = (
+                "Too many arguments provided."
+                if too_many
+                else "Not all arguments provided."
+            )
+            raise ValueError(msg)
 
         # Convert all arguments to positional arguments in correct order
         positional = list(args) + convert_kwargs_to_args(kwargs, list(parameters))
@@ -178,8 +142,17 @@ def allow_args(func):
 
         return func(*positional_only, **kwargs)
 
-    allow_args_wrapper.__signature__ = new_signature
-    return allow_args_wrapper
+    # This raises a mypy error but is perfectly fine to do. See
+    # https://github.com/python/mypy/issues/12472
+    allow_args_wrapper.__signature__ = new_signature  # type: ignore[attr-defined]
+
+    # We cast to F here to signal mypy that the return type is the same as the input
+    # type. This ignores the change of parameters from positional to keyword-only
+    # arguments.
+    # TODO(@timmens): Remove this cast once we find an explicit way to specify the
+    # change from positional to keyword-only parameter in the function signature
+    # https://github.com/OpenSourceEconomics/lcm/issues/80.
+    return cast(F, allow_args_wrapper)
 
 
 # ======================================================================================
@@ -252,12 +225,3 @@ def convert_kwargs_to_args(kwargs: dict[str, Any], parameters: list[str]) -> lis
     """
     sorted_kwargs = dict(sorted(kwargs.items(), key=lambda kw: parameters.index(kw[0])))
     return list(sorted_kwargs.values())
-
-
-def _error_message(
-    args: tuple[Any],
-    kwargs: dict[str, Any],
-    parameters: list[str],
-) -> str:
-    too_many = len(args) + len(kwargs) > len(parameters)
-    return "Too many arguments provided." if too_many else "Not all arguments provided."
