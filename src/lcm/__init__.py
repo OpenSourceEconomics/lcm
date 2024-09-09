216a583692bae7c76d67f65e1c48649659c5a295
Tim Mensinger <mensingertim@gmail.com>
2024-09-09 13:43:59 +0200

    Import pdbp in __init__.py

--- src/lcm/__init__.py
+++ src/lcm/__init__.py
@@ -1,3 +1,10 @@
+import contextlib
+
+try:
+    import pdbp  # noqa: F401
+except ImportError:
+    contextlib.suppress(Exception)
+
 from lcm import mark
 from lcm.grids import DiscreteGrid, LinspaceGrid, LogspaceGrid
 from lcm.model import Model
