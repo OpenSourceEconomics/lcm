227b85945ff4a3aa29cae5df91930341763a70cc
Tim Mensinger <mensingertim@gmail.com>
2024-09-19 16:20:48 +0200

    Test [1, 0] discrete grid case

--- tests/input_processing/test_discrete_state_conversion.py
+++ tests/input_processing/test_discrete_state_conversion.py
@@ -43,20 +43,21 @@ def model(category_class_factory):
             "a": DiscreteGrid(category_class_factory([0, 1])),
         },
         states={
-            "c": DiscreteGrid(category_class_factory([1, 10])),
+            # unsorted indices cannot be treated as indices
+            "c": DiscreteGrid(category_class_factory([1, 0])),
         },
     )
 
 
 def test_get_index_to_label_func():
-    codes_array = jnp.array([1, 10])
+    codes_array = jnp.array([1, 0])
     got = _get_index_to_code_func(codes_array, name="foo")
     assert got(__foo_index__=0) == 1
-    assert got(1) == 10
+    assert got(1) == 0
 
 
 def test_get_code_to_index_func():
-    codes_array = jnp.array([1, 10])
+    codes_array = jnp.array([1, 0])
     got = _get_code_to_index_func(codes_array, name="foo")
     assert_array_equal(got(foo=codes_array), jnp.arange(2))
 
@@ -78,4 +79,4 @@ def test_convert_discrete_codes_to_indices(model):
     assert got.states["__c_index__"].categories == ["__cat0_index__", "__cat1_index__"]
     assert got.states["__c_index__"].codes == [0, 1]
     assert got.functions["c"](0) == 1
-    assert got.functions["c"](1) == 10
+    assert got.functions["c"](1) == 0
